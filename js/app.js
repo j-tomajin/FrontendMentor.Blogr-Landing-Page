b8aefc080586d5118c7dd42b9ba9205e5a868f9f
Jerickson Tomajin <tomajinj@gmail.com>
2022-12-11 23:15:24 +0800

    added dropdown behavior

--- js/app.js
+++ js/app.js
@@ -12,21 +12,47 @@ navMenu.addEventListener('click', () => {
     }
 })
 
+// DROPDOWN
 const productBtn = document.getElementById('product-btn')
 const companyBtn = document.getElementById('company-btn')
 const connectBtn = document.getElementById('connect-btn')
 
 productBtn.addEventListener('click', (e) => {
     e.stopPropagation();
-    toggleDropDown();
+    toggleDropDown(productBtn)
 })
 
 companyBtn.addEventListener('click', (e) => {
     e.stopPropagation();
-    toggleDropDown();
+    toggleDropDown(companyBtn);
 })
 
 connectBtn.addEventListener('click', (e) => {
     e.stopPropagation();
-    toggleDropDown();
+    toggleDropDown(connectBtn);
 })
+
+const toggleDropDown = element => {
+    const Parent = element.parentElement
+    const dropdown = Parent.querySelector('.dropdown')
+
+    dropdown.classList.toggle('show-dropdown')
+    Parent.classList.toggle('btn-toggle')
+}
+
+document.documentElement.addEventListener("click", function () {
+    const dropdownall = document.querySelectorAll('.show-dropdown')
+    const btntoggle = document.querySelectorAll('.btn-toggle')
+
+    dropdownall.forEach(element => {
+        element.classList.remove('show-dropdown')
+
+        nav.classList.remove('nav-show')
+
+        btntoggle.forEach(element => {
+            element.classList.remove('btn-toggle')
+        })
+
+        menu.src = './images/icon-hamburger.svg'
+    })
+});
